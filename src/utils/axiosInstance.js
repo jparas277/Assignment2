1dedcd7e6e2a17a471cce5fcf8949f6d38e99a40
jparas277 <57638397+jparas277@users.noreply.github.com>
2023-04-26 12:00:16 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance =axios.create({
-  baseURL : "http://localhost:8081/",
+  baseURL : "http://kubernetes-alb-982615822.us-east-1.elb.amazonaws.com/",
 })
 
 export default instance;
